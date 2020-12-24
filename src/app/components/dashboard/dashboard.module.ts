06867f09895a352c6c652b41823e36f30315fdfb
HoangCaoPhi <hoangcaophi11061999@gmail.com>
2020-12-24 23:30:51 +0700

    Tiep tuc ve giao dien phan noi dung dich

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -5,13 +5,16 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ContentTranslateComponent } from './content-translate/content-translate.component';
 import { ActionTranslateComponent } from './action-translate/action-translate.component';
+import { DxButtonModule, DxTextAreaModule } from 'devextreme-angular';
 
 
 @NgModule({
   declarations: [DashboardComponent, ContentTranslateComponent, ActionTranslateComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    DxButtonModule,
+    DxTextAreaModule
   ]
 })
 export class DashboardModule { }

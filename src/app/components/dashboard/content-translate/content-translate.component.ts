ccfbd30b1d7e16a2acc978cfe958366de1ec149a
HoangCaoPhi <hoangcaophi11061999@gmail.com>
2021-01-04 22:32:00 +0700

    Chinh sua giao dien o lich su va tu da luu, sua chieu cao tu dong o text-area

--- src/app/components/dashboard/content-translate/content-translate.component.ts
+++ src/app/components/dashboard/content-translate/content-translate.component.ts
@@ -1,18 +1,25 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-content-translate',
   templateUrl: './content-translate.component.html',
-  styleUrls: ['./content-translate.component.scss']
+  styleUrls: ['./content-translate.component.scss'],
 })
 export class ContentTranslateComponent implements OnInit {
+  //#region Properties
 
-  constructor() { }
+  @ViewChild('content', {static: true})
+  inputContent: any;
+
+  //#endregion
+
+  //#region Life Cycle
+  constructor() {}
 
   ngOnInit(): void {
-    
-  
+    setTimeout(() => {
+      this.inputContent.instance.focus();
+    }, 500);
   }
-
+  //#endregion
 }
